209152b4ec457de5f65ad26c0955682273f4f135
webmobilidev <webmobilisviluppo@MacBook-Air-di-Luca.local>
2017-06-30 10:43:42 +0200

    prepare to phonegap

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -59,99 +59,6 @@ module.exports = [
         ]
     },
     {
-        "id": "cordova-plugin-contacts.contacts",
-        "file": "plugins/cordova-plugin-contacts/www/contacts.js",
-        "pluginId": "cordova-plugin-contacts",
-        "clobbers": [
-            "navigator.contacts"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.Contact",
-        "file": "plugins/cordova-plugin-contacts/www/Contact.js",
-        "pluginId": "cordova-plugin-contacts",
-        "clobbers": [
-            "Contact"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.convertUtils",
-        "file": "plugins/cordova-plugin-contacts/www/convertUtils.js",
-        "pluginId": "cordova-plugin-contacts"
-    },
-    {
-        "id": "cordova-plugin-contacts.ContactAddress",
-        "file": "plugins/cordova-plugin-contacts/www/ContactAddress.js",
-        "pluginId": "cordova-plugin-contacts",
-        "clobbers": [
-            "ContactAddress"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.ContactError",
-        "file": "plugins/cordova-plugin-contacts/www/ContactError.js",
-        "pluginId": "cordova-plugin-contacts",
-        "clobbers": [
-            "ContactError"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.ContactField",
-        "file": "plugins/cordova-plugin-contacts/www/ContactField.js",
-        "pluginId": "cordova-plugin-contacts",
-        "clobbers": [
-            "ContactField"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.ContactFindOptions",
-        "file": "plugins/cordova-plugin-contacts/www/ContactFindOptions.js",
-        "pluginId": "cordova-plugin-contacts",
-        "clobbers": [
-            "ContactFindOptions"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.ContactName",
-        "file": "plugins/cordova-plugin-contacts/www/ContactName.js",
-        "pluginId": "cordova-plugin-contacts",
-        "clobbers": [
-            "ContactName"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.ContactOrganization",
-        "file": "plugins/cordova-plugin-contacts/www/ContactOrganization.js",
-        "pluginId": "cordova-plugin-contacts",
-        "clobbers": [
-            "ContactOrganization"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.ContactFieldType",
-        "file": "plugins/cordova-plugin-contacts/www/ContactFieldType.js",
-        "pluginId": "cordova-plugin-contacts",
-        "merges": [
-            ""
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.contacts-ios",
-        "file": "plugins/cordova-plugin-contacts/www/ios/contacts.js",
-        "pluginId": "cordova-plugin-contacts",
-        "merges": [
-            "navigator.contacts"
-        ]
-    },
-    {
-        "id": "cordova-plugin-contacts.Contact-iOS",
-        "file": "plugins/cordova-plugin-contacts/www/ios/Contact.js",
-        "pluginId": "cordova-plugin-contacts",
-        "merges": [
-            "Contact"
-        ]
-    },
-    {
         "id": "es6-promise-plugin.Promise",
         "file": "plugins/es6-promise-plugin/www/promise.js",
         "pluginId": "es6-promise-plugin",
@@ -177,8 +84,6 @@ module.exports.metadata =
     "cordova-plugin-inappbrowser": "1.5.0",
     "cordova-plugin-network-information": "1.2.1",
     "cordova-plugin-statusbar": "2.2.3",
-    "cordova-plugin-compat": "1.1.0",
-    "cordova-plugin-contacts": "2.3.1",
     "es6-promise-plugin": "4.1.0",
     "cordova-plugin-x-socialsharing": "5.1.8"
 };
